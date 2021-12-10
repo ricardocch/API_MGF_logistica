a4790bd1e7980d253e6b08f3b1de0b502e4fbc37
Ricardo HP <ricardo_azul4@yahoo.com>
2021-12-09 19:14:20 -0600

    se crea endpoint para borrar usuarios y se accede a la bd para verificar usuario y contraseña

---
+++ insert user.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users(
+	name, password, email, admin, phone, cuit)
+	VALUES ('Ricardo', '$2b$10$kI9jtHzyKNjkWooVKQCptOZTqJAJ9R8edUW7oMknDeIiA9DABsAUm', 'richard@com.com', true, '55698989', 'tres');
