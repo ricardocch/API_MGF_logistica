7f503f62fea66bb8f0da93dc8d92d8be91430add
Wenceslaorojas <wenceslao.rojar@gmail.com>
2021-12-11 03:48:36 -0300

    "listo"

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const router = Router();
 const user = require("./users/User");
 const modify = require("./users/updatepass");
 const deleted = require("./users/deleteuser");
-router.get("/types", function (req, res) {});
 router.use("/create", user);
 router.use("/updatepass", modify);
 router.use("/deleteuser", deleted);
