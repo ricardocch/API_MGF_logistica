a3a7c94213979b725c2826bb6cc1bcb9ab174edf
Wenceslaorojas <wenceslao.rojar@gmail.com>
2021-12-31 00:29:51 -0300

    asdasd

--- src/routes/posteos/list-post.js
+++ src/routes/posteos/list-post.js
@@ -6,7 +6,7 @@ router.get("/", async function (req, res) {
   const { user, admin } = req.query;
   try {
     if (admin === true) {
-      let posts = await Post.findAll({ include: [User, Driver, LicensePlate] });
+      let posts = await Post.findAll();
       return res.json(posts);
     }
     let postsUser = await Post.findAll({
@@ -15,7 +15,7 @@ router.get("/", async function (req, res) {
     });
     return res.json(postsUser);
   } catch (err) {
-    res.status(500).send({ err });
+    res.status(500).send({ err: err, msg: "Algo sucedió en la ruta" });
   }
 });
 
